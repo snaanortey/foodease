73092b31ac9afce36eb63f1987c157788a7b490d
Naa Norkor <nortey.sarah@gmail.com>
2022-11-22 14:47:43 -0500

    Typescript conversion for GetRecipes page

---
+++ client/src/types.tsx
@@ -0,0 +1,11 @@
+export interface Recipes  {
+
+    id: string;
+    imageUrl: string;
+    ingredients: string[];
+    picture_link?: string | null;
+    instructions: string;
+    title: string;
+
+}
+
