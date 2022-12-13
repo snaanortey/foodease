b28a098607f8939b50c7f5b52769605383c68444
Naa Norkor <nortey.sarah@gmail.com>
2022-12-13 19:15:50 +0000

    CAP-26 Created user login route using passport js

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,10 @@ import imageSearch from "../src/routes/imageSearch";
 import ingredientsToRecipes from "../src/routes/ingredientsToRecipes";
 import searchAndSuggestMeals from "../src/routes/searchAndSuggestMeals";
 import userRegistration from "../src/routes/userRegistration";
- 
+import userLogin from "../src/routes/login";
+import secret from "../src/routes/secret";
+import passport from "passport";
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -20,5 +23,9 @@ app.use("/image/ingredients", imageSearch);
 app.use("/recipes/search", ingredientsToRecipes);
 app.use("/meals/search", searchAndSuggestMeals);
 app.use("/register", userRegistration);
+app.use("/login", userLogin);
 
-export {app};
+// Plug in the JWT strategy as a middleware so only verified users can access this route.
+app.use("/secret", passport.authenticate("jwt", { session: false }), secret);
+
+export { app };
---
+++ server/src/routes/login.ts
@@ -0,0 +1,90 @@
+import { config } from "dotenv";
+config();
+import express from "express";
+import passport from "passport";
+import { Strategy as LocalStrategy, VerifyFunction } from "passport-local";
+import bcrypt from "bcrypt";
+import { UserService } from "../services/userService";
+import { EntityNotFoundError } from "typeorm";
+import jwt from "jsonwebtoken";
+import { Strategy, ExtractJwt } from "passport-jwt";
+
+const router = express.Router();
+
+const userService = new UserService();
+
+// This verify function takes the user's email and password as reqs in the API endpoint and
+// confirms that username exists in db, and also that password matches user's hashed password
+const verify: VerifyFunction = function (email, password, callback) {
+  //Fetch user from database
+  const userDataPromise = userService.findByEmail(email);
+  userDataPromise
+    .then((userData) => {
+      bcrypt
+        .compare(password, userData.passwordHash)
+        .then((matchPassword) => {
+          if (!matchPassword) {
+            return callback(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
+          return callback(null, userData);
+        })
+        .catch((err) => {
+          return callback(err);
+        });
+    })
+    // findByEmail threw an error
+    .catch((err) => {
+      // if  user not found
+      if (err instanceof EntityNotFoundError) {
+        return callback(null, false, {
+          message: "Incorrect username or password.",
+        });
+      }
+      // other arbitrary error
+      return callback(err);
+    });
+};
+
+passport.use(new LocalStrategy(verify));
+
+// Returns a token to a user if login is successful
+router.post("/", async (req, res, next) => {
+  passport.authenticate("local", async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error("An error has occured");
+        return next(error);
+      }
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+        const body = { id: user.id, email: user.email };
+        const token = jwt.sign({ user: body }, "heyou");
+        return res.json({ token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
+// Extracts JWT token from header of http request and checks that the token had been signed
+// with the correct secretOrKey. If confirmed, user details are passed to the next middleware
+passport.use(
+  new Strategy(
+    {
+      secretOrKey: "heyou",
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (token: any, done: any): Promise<any> => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+export default router;
